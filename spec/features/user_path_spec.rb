b575b6ece2a86e8833c2f3fbab84c94d86c47edc
Luke Bartel <bartelluke@gmail.com>
2014-08-25 09:33:26 -0600

    user can visit home page and see title and tagline

--- spec/features/user_path_spec.rb
+++ spec/features/user_path_spec.rb
@@ -1,5 +1,18 @@
 feature "user path" do
   scenario "user can visit home page" do
+    visit "/"
+    expect(page).to have_content "Cageflix Netflix for Nic Cage movies"
+
+    click_on "Add Movie"
+
+    fill_in "Name", :with => "The Wickerman"
+    fill_in "Year", :with => "2006"
+    fill_in "Synopsis", :with => "Awesome Tail"
+
+    click_on "Create Movie"
+
+  
+
 
   end
 end
